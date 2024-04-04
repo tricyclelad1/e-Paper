ea4c11dae7cb0f5802e7aefe06f1ed38259fc3ff
tricyclelad1 <157566281+tricyclelad1@users.noreply.github.com>
2024-04-03 21:41:15 -0600

    Update Trophy.py

--- RaspberryPi_JetsonNano/python/examples/Trophy.py
+++ RaspberryPi_JetsonNano/python/examples/Trophy.py
@@ -38,10 +38,10 @@ try:
 
     draw = ImageDraw.Draw(Himage)
     draw.rectangle((0,0,800,480), fill=0)
-    draw.rectangle((100,0,200,35), fill=255)
-    draw.rectangle((0,400,300,170), fill=255)
-    draw.text((100, 0), 'Twilight Imperium', font = font35, fill = 255)
-    draw.text((0, 400), 'PAX MAGNIFICA BELLUM GLORIOSUM', font = font35, fill = 255)
+    draw.rectangle((100,0,400,35), fill=255)
+    draw.rectangle((0,400,700,435), fill=255)
+    draw.text((100, 0), 'Twilight Imperium', font = font35, fill = 0)
+    draw.text((0, 400), 'PAX MAGNIFICA BELLUM GLORIOSUM', font = font35, fill = 0)
     epd.display(epd.getbuffer(Himage))
 
     #time.sleep(2)
