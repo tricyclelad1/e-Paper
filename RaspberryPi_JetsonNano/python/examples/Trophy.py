2c0d888f810c9ac268871474856b6fd5828fa5af
tricyclelad1 <157566281+tricyclelad1@users.noreply.github.com>
2024-04-04 15:25:14 -0600

    Image tests
    more image tests

--- RaspberryPi_JetsonNano/python/examples/Trophy.py
+++ RaspberryPi_JetsonNano/python/examples/Trophy.py
@@ -33,8 +33,9 @@ try:
     Himage = Image.new('1', (epd.width, epd.height), 255)  # 255: clear the frame
 
     #background = Image.open(os.path.join(picdir, 'Background Test.jpg'))
-    background = Image.open(os.path.join(picdir, 'Background Test Blocking.jpg'))
+    #background = Image.open(os.path.join(picdir, 'Background Test Blocking.jpg'))
     #background = Image.open(os.path.join(picdir, 'Turtles.jpg'))
+    background = Image.open(os.path.join(picdir, 'portrait background.jpg'))
     Himage.paste(background, (0,0))
     epd.display_Fast(epd.getbuffer(Himage))
     #time.sleep(2)
