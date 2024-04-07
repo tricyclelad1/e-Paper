a8ac2af15d6725f4072a66194894487a38bbab37
tricyclelad1 <157566281+tricyclelad1@users.noreply.github.com>
2024-04-06 19:53:34 -0600

    Update Trophy.py

--- RaspberryPi_JetsonNano/python/examples/Trophy.py
+++ RaspberryPi_JetsonNano/python/examples/Trophy.py
@@ -80,7 +80,7 @@ try:
     time.sleep(2)
 
     draw = ImageDraw.Draw(Himage)
-    draw.text((400, 112), winnerName + " - " + winnerTitle, font = font44, fill = 0, anchor="mm") #400x195 is center of winner box
+    draw.text((400, 112), winnerName + " - " + winnerTitle, font = font40, fill = 0, anchor="mm") #400x195 is center of winner box
 
     epd.display(epd.getbuffer(Himage))
     time.sleep(2)
