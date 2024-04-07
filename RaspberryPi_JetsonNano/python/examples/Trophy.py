a6d4ed2f530cea1f3c7a8496916d503fb3e7d0a0
tricyclelad1 <157566281+tricyclelad1@users.noreply.github.com>
2024-04-06 20:46:59 -0600

    Update Trophy.py

--- RaspberryPi_JetsonNano/python/examples/Trophy.py
+++ RaspberryPi_JetsonNano/python/examples/Trophy.py
@@ -98,13 +98,13 @@ try:
     draw = ImageDraw.Draw(Himage)
     draw.text((400, 112), winnerName + " - " + winnerTitle, font = winnerFont, fill = 0, anchor="mm") #400x195 is center of winner box
 
-    draw.text((254, 188), player2Name + " - " + player2Faction, font = runnerUpFont, fill = 0, anchor="lm") #400x195 is center of winner box
-    draw.text((254, 231), player3Name + " - " + player3Faction, font = runnerUpFont, fill = 0, anchor="lm") #400x195 is center of winner box
-    draw.text((254, 275), player4Name + " - " + player4Faction, font = runnerUpFont, fill = 0, anchor="lm") #400x195 is center of winner box
-    draw.text((254, 318), player5Name + " - " + player5Faction, font = runnerUpFont, fill = 0, anchor="lm") #400x195 is center of winner box
-    draw.text((254, 361), player6Name + " - " + player6Faction, font = runnerUpFont, fill = 0, anchor="lm") #400x195 is center of winner box
-    draw.text((254, 404), player7Name + " - " + player7Faction, font = runnerUpFont, fill = 0, anchor="lm") #400x195 is center of winner box
-    draw.text((254, 447), player8Name + " - " + player8Faction, font = runnerUpFont, fill = 0, anchor="lm") #400x195 is center of winner box
+    draw.text((250, 188), player2Name + " - " + player2Faction, font = runnerUpFont, fill = 0, anchor="lm") #400x195 is center of winner box
+    draw.text((250, 231), player3Name + " - " + player3Faction, font = runnerUpFont, fill = 0, anchor="lm") #400x195 is center of winner box
+    draw.text((250, 275), player4Name + " - " + player4Faction, font = runnerUpFont, fill = 0, anchor="lm") #400x195 is center of winner box
+    draw.text((250, 318), player5Name + " - " + player5Faction, font = runnerUpFont, fill = 0, anchor="lm") #400x195 is center of winner box
+    draw.text((250, 361), player6Name + " - " + player6Faction, font = runnerUpFont, fill = 0, anchor="lm") #400x195 is center of winner box
+    draw.text((250, 404), player7Name + " - " + player7Faction, font = runnerUpFont, fill = 0, anchor="lm") #400x195 is center of winner box
+    draw.text((250, 447), player8Name + " - " + player8Faction, font = runnerUpFont, fill = 0, anchor="lm") #400x195 is center of winner box
 
     epd.display(epd.getbuffer(Himage))
     time.sleep(2)
